7f09d0ca8df53a10b3a1961ec0c89cfccc6c4d82
Glitch (dashboard-neha) <none>
2020-06-20 15:20:06 +0000

    🍇🍎 Updated with Glitch

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const fs = require("fs");
+var path = require('path');
 app.use(bodyParser.urlencoded({limit: '20mb', extended: true}))
 app.use(bodyParser.json({limit: '20mb', extended: true}));
 
@@ -10,7 +11,7 @@ app.use(bodyParser.json({limit: '20mb', extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + "/views/login.html");
 });
 app.post("/upload",(request,response)=>
        {
@@ -27,7 +28,15 @@ app.post("/upload",(request,response)=>
 app.get("/callit",(req,res)=>{
   res.sendFile(__dirname+"/sets/data.json");
 })
-
+app.post("/check",(req,res)=>{
+  console.log(req.body);
+  if(req.body.pass==process.env.PASSWORD)
+    {
+      res.sendFile(__dirname + "/views/index.html")
+    }
+  else
+    res.sendFile(__dirname + "/views/login.html")
+})
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
